b431ee54ffe17bd5a3dc7ca7049edd57489a0e46
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-05 06:35:08 +0500

    Extend `CacheClientInterface` with additional cache operation methods (`set`, `get`, `has`, `delete`).

--- src/Cache/CacheClientInterface.php
+++ src/Cache/CacheClientInterface.php
@@ -12,9 +12,22 @@ use Charcoal\Contracts\Storage\StorageProviderInterface;
 
 /**
  * Represents a contract for cache clients.
- * It extends the StorageProviderInterface, which provides
- * a common interface for storage providers.
+ * It extends the StorageProviderInterface,
+ * which provides a common interface for storage providers.
  */
 interface CacheClientInterface extends StorageProviderInterface
 {
+    public function set(string $key, mixed $value, ?int $ttl = null, ?bool $withChecksum = null): mixed;
+
+    public function get(
+        string $key,
+        bool   $returnEnvelope = false,
+        bool   $returnReferenceKeyObject = true,
+        bool   $expectInteger = false,
+        ?bool  $verifyChecksum = null
+    ): int|string|null|array|object|bool;
+
+    public function has(string $key): bool;
+
+    public function delete(string $key): bool;
 }
